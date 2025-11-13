8eb6bc05e9acf88ed78c78fae004013dd9b8b47e
Md Akram <mdakram12022002@gmail.com>
2025-11-14 01:40:03 +0530

    fix: update API URLs for production deployment

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import './styles/globals.css';
+import "./styles/globals.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
+import ErrorBoundary from "./ErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <App />
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
